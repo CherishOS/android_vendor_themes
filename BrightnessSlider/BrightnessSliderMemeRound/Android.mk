486697034ab204025f02bc439131f6ae6029e620
SuperDroidBond <superdroidbond@yahoo.com>
2021-06-07 00:56:37 +0530

    BrightnessSliderStyles: update for RRO packages
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- BrightnessSlider/BrightnessSliderMemeRound/Android.mk
+++ BrightnessSlider/BrightnessSliderMemeRound/Android.mk
@@ -15,12 +15,11 @@
 #
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-CAL_MODULE_TAGS := optional
 
+LOCAL_RRO_THEME := BrightnessSliderMemeRound
 LOCAL_PACKAGE_NAME := BrightnessSliderMemeRound
-LOCAL_SDK_VERSION := current
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := false
 LOCAL_PRODUCT_MODULE := true
-
-include $(BUILD_PACKAGE)
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_SDK_VERSION := current
+include $(BUILD_RRO_PACKAGE)
