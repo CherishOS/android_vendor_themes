ab38526f348f918f08b2d1c25585fe1c7d28a05a
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-11-24 13:55:25 +0700

    themes: nuke navbar

--- themes.mk
+++ themes.mk
@@ -3,12 +3,3 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
 
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
-
-# Navbar
-PRODUCT_PACKAGES += \
-    GesturalNavigationOverlayLong \
-    GesturalNavigationOverlayMedium \
-    GesturalNavigationOverlayHidden \
-    GesturalNavigationRadiusLow \
-    GesturalNavigationRadiusVeryLow \
-    GesturalNavigationRadiusHidden
