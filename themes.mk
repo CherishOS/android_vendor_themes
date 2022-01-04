4be2e4d55cba6755879e8a5e5256d71d80233d0b
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-01-04 22:58:55 +0700

    themes:Build overlay backgrounds
    removed icon packages because included in vendor/cherish
    remove navbar packages
    only build overlay for themes backgrounds now
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- themes.mk
+++ themes.mk
@@ -3,3 +3,25 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
 
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
+
+# Custom Overlays
+# Settings
+PRODUCT_PACKAGES += \
+    SystemRavenBlackOverlay \
+    SystemUIRavenBlackOverlay \
+    SystemDarkGrayOverlay \
+    SystemUIDarkGrayOverlay \
+    SystemStyleOverlay \
+    SystemUIStyleOverlay \
+    SystemNightOverlay \
+    SystemUINightOverlay \
+    SystemMaterialOceanOverlay \
+    SystemUIMaterialOceanOverlay \
+    SystemBakedGreenOverlay \
+    SystemUIBakedGreenOverlay \
+    SystemChocoXOverlay \
+    SystemUIChocoXOverlay \
+    SystemDarkAubergineOverlay \
+    SystemUIDarkAubergineOverlay \
+    SystemClearSpringOverlay \
+    SystemUIClearSpringOverlay
