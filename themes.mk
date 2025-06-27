bf69b47916d83aa383f37b32918e981c23561ef0
Hưng Phan <phandinhhungvp2001@gmail.com>
2025-06-27 19:20:56 +0700

    themes:update to Android 16
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- themes.mk
+++ themes.mk
@@ -1,126 +1,480 @@
 #
-# Copyright (C) 2020-22 The CherishOS Project
+# Copyright (C) 2018 The LineageOS Project
+#               2017-2024 crDroid Android Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-# Data icons
+# Accord
+TARGET_INCLUDE_ACCORD ?= true
+ifeq ($(TARGET_INCLUDE_ACCORD),true)
 PRODUCT_PACKAGES += \
-    AuroraDataOverlay \
-    FaintUIDataOverlay \
-    GradiconDataOverlay \
-    LornDataOverlay \
-    PlumpyDataOverlay
+    Accord
+endif
 
-# Icon Data
+# Clocks (SystemUI)
 PRODUCT_PACKAGES += \
-    IconDataFive \
-    IconDataFour \
-    IconDataOne \
-    IconDataThree \
-    IconDataTwo
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap \
+    SystemUIClocks-Weather
 
-# Icon shapes
+# Fonts
 PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    ClockFontACFilmstripOverlay \
+    ClockFontAccuratistOverlay \
+    ClockFontAclonicaOverlay \
+    ClockFontAlmonteSnowOverlay \
+    ClockFontAlphaCloudsOverlay \
+    ClockFontAlphaFlowersOverlay \
+    ClockFontAlphaWoodOverlay \
+    ClockFontAmaranteOverlay \
+    ClockFontAmpad3D2Overlay \
+    ClockFontBariolOverlay \
+    ClockFontBetsyFlanaganOverlay \
+    ClockFontBigCheeseOverlay \
+    ClockFontBrandayolqOverlay \
+    ClockFontBudmoJigglerOverlay \
+    ClockFontBunnyRabbitsOverlay \
+    ClockFontCFBadNewsOverlay \
+    ClockFontCFOneTwoTreesOverlay \
+    ClockFontCagliostroOverlay \
+    ClockFontCatOverlay \
+    ClockFontCoconOverlay \
+    ClockFontComfortaaOverlay \
+    ClockFontComicSansOverlay \
+    ClockFontConcentrateOverlay \
+    ClockFontCookieRunOverlay \
+    ClockFontCoolstoryOverlay \
+    ClockFontCrackmanOverlay \
+    ClockFontDiscoMidnightOverlay \
+    ClockFontEasterBunnyOverlay \
+    ClockFontEditPointsFilledOverlay \
+    ClockFontEditPointsOverlay \
+    ClockFontElriott2Overlay \
+    ClockFontExotwoOverlay \
+    ClockFontFibographyOverlay \
+    ClockFontFifa2018Overlay \
+    ClockFontFloorlightOverlay \
+    ClockFontGautsMotelUpperRightOverlay \
+    ClockFontGoogleSansFlexOverlay \
+    ClockFontGrandHotelOverlay \
+    ClockFontHangedOverlay \
+    ClockFontHarmonySansOverlay \
+    ClockFontHotSweatOverlay \
+    ClockFontKGOnlyHopeOverlay \
+    ClockFontKaramuruhOverlay \
+    ClockFontKingthingsOverlay \
+    ClockFontLMSCliffordOverlay \
+    ClockFontLatoOverlay \
+    ClockFontLinotteOverlay \
+    ClockFontLittleBunnyOverlay \
+    ClockFontLowerAtmosphereOverlay \
+    ClockFontMessingLetternOverlay \
+    ClockFontMonbijouxClownpieceOverlay \
+    ClockFontNeonDiscoOverlay \
+    ClockFontNinjasOverlay \
+    ClockFontNokiaPureOverlay \
+    ClockFontNothingDotHeadlineOverlay \
+    ClockFontNunitoOverlay \
+    ClockFontOneplusSansOverlay \
+    ClockFontOneplusSlateOverlay \
+    ClockFontOswaldOverlay \
+    ClockFontPinewoodOverlay \
+    ClockFontPlaidEventOverlay \
+    ClockFontPlantsLettersOverlay \
+    ClockFontPlayOverlay \
+    ClockFontPoppinsSourceOverlay \
+    ClockFontQuandoOverlay \
+    ClockFontQuickSouthOverlay \
+    ClockFontRedressedOverlay \
+    ClockFontReemKufiOverlay \
+    ClockFontRemponkOverlay \
+    ClockFontRobotoCondensedOverlay \
+    ClockFontRomantiquesOverlay \
+    ClockFontRoundheadsOverlay \
+    ClockFontRubikOverlay \
+    ClockFontSamsungOneOverlay \
+    ClockFontSansSerifOverlay \
+    ClockFontScrapItUpOverlay \
+    ClockFontSonySketchOverlay \
+    ClockFontSpaceGameOverlay \
+    ClockFontStandardHeaderOverlay \
+    ClockFontStoropiaOverlay \
+    ClockFontSurferOverlay \
+    ClockFontTh3machineOverlay \
+    ClockFontUbuntuOverlay \
+    ClockFontVtksdura3dOverlay \
+    ClockFontZnikomitNo24Overlay \
+    ClockFontIOSOverlay \
+    ClockFontHerculesOverlay \
+    ClockFontSlimOverlay \
+    ClockFontNtype82Overlay \
+    ClockFontSubwayOverlay \
+    ClockFontMotorola \
+    FontAccuratistOverlay \
+    FontAclonicaOverlay \
+    FontAmaranteOverlay \
+    FontBariolOverlay \
+    FontCagliostroOverlay \
+    FontCoconOverlay \
+    FontComfortaaOverlay \
+    FontComicSansOverlay \
+    FontCookieRunOverlay \
+    FontCooljazzOverlay \
+    FontCoolstoryOverlay \
+    FontExotwoOverlay \
+    FontEvoSansOverlay \
+    FontEvolveSansOverlay \
+    FontFifa2018Overlay \
+    FontGrandHotelOverlay \
+    FontGeneralSansOverlay \
+    FontGoogleSansFlexOverlay \
+    FontHarmonySansOverlay \
+    FontLatoOverlay \
+    FontLinotteOverlay \
+    FontNokiaPureOverlay \
+    FontNothingDotHeadlineOverlay \
+    FontNothingDotOverlay \
+    FontNunitoOverlay \
+    FontOneUISansOverlay \
+    FontOneplusSansOverlay \
+    FontOneplusSlateOverlay \
+    FontPoppinsSourceOverlay \
+    FontOswaldOverlay \
+    FontPlayOverlay \
+    FontQuandoOverlay \
+    FontRedressedOverlay \
+    FontReemKufiOverlay \
+    FontRobotoCondensedOverlay \
+    FontRookeryOverlay \
+    FontRubikOverlay \
+    FontSamsungOneOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
+    FontSansSerifOverlay \
+    FontSansSerifProOverlay \
+    FontSonySketchOverlay \
+    FontStoropiaOverlay \
+    FontSurferOverlay \
+    FontUbuntuOverlay \
+    FontVolteOverlay
+
+# Icon Packs
+PRODUCT_PACKAGES += \
+    IconPackAcherusAndroidOverlay \
+    IconPackAcherusLauncherOverlay \
+    IconPackAcherusSettingsOverlay \
+    IconPackAcherusSystemUIOverlay \
+    IconPackAuroraAndroidOverlay \
+    IconPackAuroraSystemUIOverlay \
+    IconPackCircularAndroidOverlay \
+    IconPackCircularLauncherOverlay \
+    IconPackCircularSettingsOverlay \
+    IconPackCircularSystemUIOverlay \
+    IconPackCircularThemePickerOverlay \
+    IconPackFilledAndroidOverlay \
+    IconPackFilledLauncherOverlay \
+    IconPackFilledSettingsOverlay \
+    IconPackFilledSystemUIOverlay \
+    IconPackFilledThemePickerOverlay \
+    IconPackGradiconAndroidOverlay \
+    IconPackGradiconSystemUIOverlay \
+    IconPackKaiAndroidOverlay \
+    IconPackKaiLauncherOverlay \
+    IconPackKaiSettingsOverlay \
+    IconPackKaiSystemUIOverlay \
+    IconPackKaiThemePickerOverlay \
+    IconPackLornAndroidOverlay \
+    IconPackLornSystemUIOverlay \
+    IconPackNostalgicAndroidOverlay \
+    IconPackNostalgicLauncherOverlay \
+    IconPackNostalgicSettingsOverlay \
+    IconPackNostalgicSystemUIOverlay \
+    IconPackNostalgicThemePickerOverlay \
+    IconPackOOSAndroidOverlay \
+    IconPackOOSLauncherOverlay \
+    IconPackOOSSettingsOverlay \
+    IconPackOOSSystemUIOverlay \
+    IconPackOOSThemePickerOverlay \
+    IconPackOutlineAndroidOverlay \
+    IconPackOutlineLauncherOverlay \
+    IconPackOutlineSettingsOverlay \
+    IconPackOutlineSystemUIOverlay \
+    IconPackPUIAndroidOverlay \
+    IconPackPUILauncherOverlay \
+    IconPackPUISettingsOverlay \
+    IconPackPUISystemUIOverlay \
+    IconPackPUIThemePickerOverlay \
+    IconPackPlumpyAndroidOverlay \
+    IconPackPlumpySystemUIOverlay \
+    IconPackRoundedAndroidOverlay \
+    IconPackRoundedLauncherOverlay \
+    IconPackRoundedSettingsOverlay \
+    IconPackRoundedSystemUIOverlay \
+    IconPackRoundedThemePickerOverlay \
+    IconPackSamAndroidOverlay \
+    IconPackSamLauncherOverlay \
+    IconPackSamSettingsOverlay \
+    IconPackSamSystemUIOverlay \
+    IconPackSamThemePickerOverlay \
+    IconPackVictorAndroidOverlay \
+    IconPackVictorLauncherOverlay \
+    IconPackVictorSettingsOverlay \
+    IconPackVictorSystemUIOverlay \
+    IconPackVictorThemePickerOverlay \
+    IconPackXperiaAndroidOverlay \
+    IconPackXperiaSettingsOverlay \
+    IconPackXperiaSystemUIOverlay
+
+# Icon Shapes
+PRODUCT_PACKAGES += \
+    IconShapeCloudyOverlay \
+    IconShapeCylinderOverlay \
+    IconShapeFlowerOverlay \
+    IconShapeHeartOverlay \
+    IconShapeHexagonOverlay \
     IconShapeIosOverlay \
-    IconShapeLeafyOverlay \
-    IconShapeRohieMeowOverlay \
-    IconShapeScrollOverlay
+    IconShapeLeafOverlay \
+    IconShapeMeowOverlay \
+    IconShapePebbleOverlay \
+    IconShapeRoundedHexagonOverlay \
+    IconShapeRoundedRectOverlay \
+    IconShapeSamsungOverlay \
+    IconShapeScrollOverlay \
+    IconShapeStretchedOverlay \
+    IconShapeSquareOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeVesselOverlay
 
-# Statusbar Icons
+# Signal Icons
 PRODUCT_PACKAGES += \
-    AcherusSignalOverlay \
-    DoraSignalOverlay \
-    StrokeSignalOverlay \
-    SneakySignalOverlay \
-    XperiaSignalOverlay \
-    ZigZagSignalOverlay \
-    WavySignalOverlay \
-    RoundSignalOverlay \
-    InsideSignalOverlay \
-    BarsSignalOverlay \
+    AiirOSignalOverlay \
     AquariumSignalOverlay \
+    AuroraSignalOverlay \
+    BananaSignalOverlay \
+    BarsSignalOverlay \
+    BoldSignalOverlay \
     ButterflySignalOverlay \
+    CapsuleSignalOverlay \
+    CircleSignalOverlay \
     DaunSignalOverlay \
     DecSignalOverlay \
     DeepSignalOverlay \
+    DoraSignalOverlay \
+    DottedSignalOverlay \
     EqualSignalOverlay \
+    FaintUISignalOverlay \
     FanSignalOverlay \
+    ForlornSignalOverlay \
+    GlummySignalOverlay \
     GradiconSignalOverlay \
+    HeartbeatSignalOverlay \
+    HollowSignalOverlay \
     HuaweiSignalOverlay \
+    IOSSignalOverlay \
+    InsideSignalOverlay \
+    IosSignalOverlay \
+    JapaneseSignalOverlay \
+    KoalaSignalOverlay \
+    LineDotSignalOverlay \
+    LinealSignalOverlay \
+    LinearSignalOverlay \
+    MicroWaveSignalOverlay \
+    MiniSignalOverlay \
+    NinjaSignalOverlay \
+    NothingDotSignalOverlay \
+    NumberSignalOverlay \
+    OdinSignalOverlay \
+    PillsSignalOverlay \
+    PlumpySignalOverlay \
+    ROGSignalOverlay \
     RelSignalOverlay \
+    RomanSignalOverlay \
+    RoundSignalOverlay \
+    RouterSignalOverlay \
+    ScaleSignalOverlay \
     ScrollSignalOverlay \
     SeaSignalOverlay \
+    SharpSignalOverlay \
+    SleekSignalOverlay \
+    SneakySignalOverlay \
+    SpiralSignalOverlay \
     StackSignalOverlay \
+    StrokeSignalOverlay \
+    TowerSignalOverlay \
+    WaffleSignalOverlay \
     WannuiSignalOverlay \
+    WavySignalOverlay \
     WindowsSignalOverlay \
+    WindySignalOverlay \
     WingSignalOverlay \
-    CircleSignalOverlay \
-    IosSignalOverlay \
-    MiniSignalOverlay \
-    NothingDotSignalOverlay \
-    OdinSignalOverlay \
-    OutlineSignalOverlay \
-    PUISignalOverlay \
-    PillsSignalOverlay \
-    RomanSignalOverlay 
+    XperiaSignalOverlay \
+    ZigZagSignalOverlay
 
-# Wi-Fi Icons
+# WiFi Icons
 PRODUCT_PACKAGES += \
-    AcherusWiFiOverlay \
+    AiirOWiFiOverlay \
+    AuroraWiFiOverlay \
+    BarsWiFiOverlay \
+    BoldWiFiOverlay \
+    CapsuleWiFiOverlay \
     DoraWiFiOverlay \
+    FaintUIWiFiOverlay \
+    ForlornWiFiOverlay \
+    GlummyWiFiOverlay \
     GradiconWiFiOverlay \
+    HollowWiFiOverlay \
+    InsideWiFiOverlay \
+    IosWiFiOverlay \
+    JapaneseWiFiOverlay \
+    KoalaWiFiOverlay \
+    LandscapeWiFiOverlay \
+    LineDotWiFiOverlay \
+    LinealWiFiOverlay \
+    LinearWiFiOverlay \
+    MicroWaveWiFiOverlay \
     NothingDotWiFiOverlay \
-    StrokeWiFiOverlay \
+    NumberWiFiOverlay \
+    PlumpyWiFiOverlay \
+    RoundWiFiOverlay \
+    RouterWiFiOverlay \
+    ScaleWiFiOverlay \
+    SharpWiFiOverlay \
     SneakyWiFiOverlay \
-    XperiaWiFiOverlay \
-    ZigZagWiFiOverlay \
+    SpiralWiFiOverlay \
+    StrokeWiFiOverlay \
+    TowerWiFiOverlay \
+    WaffleWiFiOverlay \
     WavyWiFiOverlay \
     WeedWiFiOverlay \
-    RoundWiFiOverlay \
-    InsideWiFiOverlay \
-    OutlineWiFiOverlay \
-    BarsWiFiOverlay
+    WindyWiFiOverlay \
+    XperiaWiFiOverlay \
+    ZigZagWiFiOverlay
+
+# Brightness slider styles
+#PRODUCT_PACKAGES += \
+#    BrightnessSliderAcunOverlay \
+#    BrightnessSliderBangOverlay \
+#    BrightnessSliderCyberpunkOverlay \
+#    BrightnessSliderFilledOverlay \
+#    BrightnessSliderGradiantOverlay \
+#    BrightnessSliderLeafyOutlineOverlay \
+#    BrightnessSliderLightyOverlay \
+#    BrightnessSliderLineOverlay \
+#    BrightnessSliderMinimalThumbOverlay \
+#    BrightnessSliderNeumorphOverlay \
+#    BrightnessSliderOldSchoolThumbOverlay \
+#    BrightnessSliderOutlineOverlay \
+#    BrightnessSliderRoundedClipOverlay \
+#    BrightnessSliderThinOverlay \
+#    BrightnessSliderThumbSliderOverlay \
+#    BrightnessSliderTranslucentOverlay
+
+# Navbar styles
+ifeq ($(WITH_GMS),true)
+PRODUCT_PACKAGES += \
+    NavbarAndroidPixelLauncherOverlay \
+    NavbarAsusPixelLauncherOverlay \
+    NavbarDoraPixelLauncherOverlay \
+    NavbarPixelLauncherOverlay \
+    NavbarMotoPixelLauncherOverlay \
+    NavbarNexusPixelLauncherOverlay \
+    NavbarOldPixelLauncherOverlay \
+    NavbarOnePlusPixelLauncherOverlay \
+    NavbarOneUiPixelLauncherOverlay \
+    NavbarSammyPixelLauncherOverlay \
+    NavbarTecnoCamonPixelLauncherOverlay
+else
+PRODUCT_PACKAGES += \
+    NavbarAndroidOverlay \
+    NavbarAsusOverlay \
+    NavbarDoraOverlay \
+    NavbarCherishOverlay \
+    NavbarMotoOverlay \
+    NavbarNexusOverlay \
+    NavbarOldOverlay \
+    NavbarOnePlusOverlay \
+    NavbarOneUiOverlay \
+    NavbarSammyOverlay \
+    NavbarTecnoCamonOverlay
+endif
+
+# QS UI Style
+#PRODUCT_PACKAGES += \
+#    A11QSUI \
+#    QSOutline \
+#    QSTwoToneAccent \
+#    QSTwoToneAccentTrans \
+#    QSShaded \
+#    QSCyberPunk \
+#    QSNeumorph \
+#    QSReflected \
+#    QSSurround \
+#    QSThin
+
+# Progress Bar Themes
+PRODUCT_PACKAGES += \
+    PGB_BlockyThumb \
+    PGB_MinimalThumb \
+    PGB_OutlineThumb \
+    PGB_Shishu
+
+# Notification Themes
+PRODUCT_PACKAGES += \
+    NotifCyberPunk \
+    NotifDuoline \
+    NotifFluid \
+    NotifIOS \
+    NotifLayers
+
+# Power Menu Themes
+PRODUCT_PACKAGES += \
+    PowerCyberPunk \
+    PowerDuoline \
+    PowerIOS \
+    PowerLayers
+
+# BetterQS
+#PRODUCT_PACKAGES += \
+#    BetterQS
 
 # Themes
 PRODUCT_PACKAGES += \
-    AndroidBlackThemeOverlay \
-    AndroidClearThemeOverlay \
-    AndroidVividThemeOverlay 
-	
-# QS Panel
-PRODUCT_PACKAGES += \
-    QSOutline \
-    QSTwoToneAccent \
-    QSTwoToneAccentTrans \
-    QSShaded \
-    QSCyberPunk \
-    QSNeumorph \
-    QSReflected \
-    QSSurround \
-    QSThin
+    AndroidBlackThemeOverlay
+
+# Udfps
+#ifeq ($(TARGET_HAS_UDFPS),true)
+#PRODUCT_PACKAGES += \
+#    UdfpsAnimations \
+#    UdfpsIcons
+#endif
+
+# Utility Overlays
+#PRODUCT_PACKAGES += \
+#    HideSmartSpace \
+#    SmartSpaceOffset \
+#    HideClock
 
 # Volume Styles
-PRODUCT_PACKAGES += \
-    VolumeDoubleLayer \
-    VolumeGradient \
-    VolumeNeumorph \
-    VolumeNeumorphOutline \
-    VolumeOutline \
-    VolumeShadedLayer \
-    VolumeAOSPRevamped
+#PRODUCT_PACKAGES += \
+#    VolumeDoubleLayer \
+#    VolumeGradient \
+#    VolumeNeumorph \
+#    VolumeNeumorphOutline \
+#    VolumeOutline \
+#    VolumeShadedLayer
 
-# QS UI Style
-PRODUCT_PACKAGES += \
-    A11QSUI \
-    ReevaluateSysUIOverlay
+# Include {Lato,Rubik} fonts
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/themes/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    $(call find-copy-subdir-files,*,vendor/themes/prebuilt/product/media/audio/ui,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
